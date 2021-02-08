61b8b51579e1ea118b35fce6ebc6c430885bc93d
Jean Ducrot <torcud@gmail.com>
2021-02-08 18:09:44 -0500

    WIP

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,14 @@
 module.exports = (config) => {
+  // Filters
+  const dateFilter = require("./src/filters/date-filter.js");
+  const w3DateFilter = require("./src/filters/w3-date-filter.js");
+
   // Set directories to pass through to the dist folder
   config.addPassthroughCopy("./src/images/");
+
+  // Add filters
+  config.addFilter("dateFilter", dateFilter);
+  config.addFilter("w3DateFilter", w3DateFilter);
 
   const sortByDisplayOrder = require("./src/utils/sort-by-display-order.js");
 
@@ -13,7 +21,12 @@ module.exports = (config) => {
   config.addCollection("featuredWork", (collection) => {
     return sortByDisplayOrder(
       collection.getFilteredByGlob("./src/work/*.md")
-    ).filter(x => x.data.featured);
+    ).filter((x) => x.data.featured);
+  });
+
+  // Returns a collection of blog posts in reverse date order
+  config.addCollection("blog", (collection) => {
+    return [...collection.getFilteredByGlob("./src/posts/*.md")].reverse();
   });
 
   return {
